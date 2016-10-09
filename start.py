6dcb9110661deb338a52ef0eeaae3a2b0b15d141
tylan <tylan@tylandeMacBook-Pro.local>
2016-10-09 14:58:49 +0800

    tylan

--- start.py
+++ start.py
@@ -3,8 +3,9 @@ from lib.execute_selection import execute_selection
 import os
 
 if __name__ == '__main__':
-	#动态生成根目录路径
+	#Save the auty path into file.
 	autyPath = os.path.dirname(__file__)
 	pathFilePath = os.path.join(autyPath,'root_path')
 	open(pathFilePath,'w').write(autyPath)
+	#execute scripts.
 	execute_selection(autyPath)
--- test.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-import scripts.scripts.check_expire_coupon
