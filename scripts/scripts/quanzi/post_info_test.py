36a75628178421ae4b3c46dc73d2485a8d112a5d
tylan <tylan@tylandeMacBook-Pro.local>
2016-10-11 15:29:57 +0800

    tylan

--- scripts/scripts/quanzi/post_info_test.py
+++ scripts/scripts/quanzi/post_info_test.py
@@ -7,6 +7,7 @@ from utils.get_lottery_session_id import get_lottery_session_id
 import json
 from utils.get_lottery_user_id import get_lottery_user_id
 from utils.utils import tylan_assert
+from utils.get_lottery_post_info import get_lottery_post_info
 
 def post_info_test(accountId,postId,apiLevel):
 	s = requests.Session()
@@ -17,14 +18,20 @@ def post_info_test(accountId,postId,apiLevel):
 		'postId':postId,
 		'apiLevel':apiLevel
 	}
+	postInfo = get_lottery_post_info(postId)
 	r = s.post(url, data = data)
-	#print r.content
-	basic_assert(r)
+	#print r
+	#print postInfo
+	return r,postInfo
 
-def basic_assert(r):
+def basic_assert(r,postInfo):
 	jsonResult = json.loads(r.content)
 	tylan_assert(jsonResult['resultDesc'],u"获取帖子信息成功")
 	tylan_assert(jsonResult['result'],100)
+	tylan_assert(str(postInfo[2]),jsonResult['post']['boardId'])
+	tylan_assert(str(postInfo[3]),jsonResult['post']['userId'])
+	tylan_assert(str(postInfo[4]),jsonResult['post']['text'])
 
 if __name__ == '__main__':
-    post_info_test('runcheck5@163.com','1132635','27')
+	r,postInfo = post_info_test('runcheck5@163.com','1132635','27')
+	basic_assert(r,postInfo)
