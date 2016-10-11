5d57940ddaf625dbe74105306f59bc28076e315c
tylan <tylan@tylandeMacBook-Pro.local>
2016-10-11 15:08:24 +0800

    tylan

--- scripts/scripts/quanzi/get_share_board_list_test.py
+++ scripts/scripts/quanzi/get_share_board_list_test.py
@@ -7,7 +7,7 @@ import json
 from utils.get_lottery_user_id import get_lottery_user_id
 from utils.utils import tylan_assert,tylan_assert_include
 
-def get_followers_test(accountId,_type):
+def get_share_board_list_test():
 	s = requests.Session()
 	url = 'http://quanzi.caipiao.163.com/circle_getShareBoardList.html'
 	r = s.post(url)
@@ -19,6 +19,8 @@ def basic_assert(r):
 	tylan_assert(jsonResult['resultDesc'],u"获取小组列表成功")
 	tylan_assert(jsonResult['result'],100)
 	tylan_assert_include(jsonResult,'boards')
+	for one in jsonResult['boards']:
+		tylan_assert(one['circle'],'1')
 
 if __name__ == '__main__':
-    basic_assert(get_followers_test('runcheck5@163.com','0'))
+    basic_assert(get_share_board_list_test())
