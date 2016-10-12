9627f7ca5598ea70af719eb53643cff107b182ea
tylan <tylan@tylandeMacBook-Pro.local>
2016-10-12 11:23:55 +0800

    tylan

--- scripts/backup/1.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from utils.get_lottery_session_id import get_lottery_session_id
-
-if __name__ == '__main__':
-	print 'sessionId:'+get_lottery_session_id()
-
--- utils/get_lottery_user_flowers_cnt.py
+++ utils/get_lottery_user_flowers_cnt.py
@@ -5,5 +5,14 @@ from .sql_info import qz_host,qz_user,qz_pwd,qz_db_name
 def get_lottery_user_flowers_cnt(userId):
 	#Get userId from mysql db ts_user_info.
 	sql = 'select * from ts_user_flower where user_id = \''+str(userId)+'\''
+	#print sql
 	r = exec_sql(sql,qz_host,qz_user,qz_pwd,qz_db_name)
+	#print r
 	return r[0][3]
+
+def get_lottery_user_recv_flower_cnt(userId):
+	sql = 'select * from ts_user_flower where user_id = \''+str(userId)+'\''
+	#print sql
+	r = exec_sql(sql,qz_host,qz_user,qz_pwd,qz_db_name)
+	#print r
+	return r[0][2]
