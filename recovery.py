0e088233281c7c698c884418d507a678c3c4a30c
bjlantianyou <bjlantianyou@corp.netease.com>
2017-01-09 18:29:39 +0800

    tylan

--- recovery.py
+++ recovery.py
@@ -3,6 +3,6 @@ import os
 from lib.recovery_code import recovery_code
 
 if __name__ == '__main__':
-	autyPath = os.getcwd()
+	autyPath = os.path.dirname(os.path.abspath(__file__))
 	#Scripts recovery.
 	recovery_code(autyPath)
--- start.py
+++ start.py
@@ -6,3 +6,6 @@ import os
 if __name__ == '__main__':
 	#Execute scripts.
 	execute_selection()
+	autyPath = os.path.dirname(os.path.abspath(__file__))
+	#Scripts recovery.
+	recovery_code(autyPath)
